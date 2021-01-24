d5fdeb0757b069224a5e8f0c1e499012c361dd6c
Aleksandra Joachimiak <joachim07@poczta.onet.pl>
2021-01-24 16:40:28 +0100

    Modify client/app.js, client/index.html; Add server.js, package.json; basic server

--- client/app.js
+++ client/app.js
@@ -6,3 +6,50 @@ const userNameInput = document.getElementById('username');
 const messageContentInput = document.getElementById('message-content');
 
 let userName = '';
+
+const login = function (e) {
+  e.preventDefault();
+  
+  if (userNameInput.value.length > 0) {
+    userName = userNameInput.value;
+    loginForm.classList.remove('show'); //login form is hidden
+    messagesSection.classList.add('show'); //message form is shown
+  } else {
+    alert("Type your name.")
+  }
+}
+loginForm.addEventListener('submit', login);
+
+const addMessage = function (author, content) {
+  const message = document.createElement('li');
+  message.classList.add('message', 'message--received');
+  
+  if (author === userName) {
+    message.classList.add('message--self');
+  }
+  message.innerHTML = `
+    <h3 class='message__author'>
+      ${userName === author ? 'You' : author}
+    </h3>
+    <div class='message__content'>
+      ${content}
+    </div>
+  `;
+  messagesList.appendChild(message);
+}
+
+const sendMessage = function (e) {
+  e.preventDefault();
+  
+  messageContentInput.value;
+  let messageContent = messageContentInput.value;
+  
+  if ((!messageContent.length)) {
+    alert('Type your message.');
+  }
+  else {
+    addMessage(userName, messageContent);
+    messageContentInput.value = '';
+  }
+}
+addMessageForm.addEventListener('submit', sendMessage);
